d2282d6a8e90bc359bf6ad9c2bcc4773878d11e3
Laurent Bihin <lolofanf1@hotmail.com>
2024-05-26 22:38:26 +0200

    initial rename KSFGolf into app

--- app/urls.py
+++ app/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from KSFGolf.views import index
+from app.views import index
 
 urlpatterns = [
     path('', index, name='home'),
+    path('accounts/', include('accounts.urls'), name='accounts'),
     path('admin/', admin.site.urls),
 ]

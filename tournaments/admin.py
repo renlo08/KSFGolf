7497884ad97e914615b2e0f3d94cda2847d20b57
Laurent Bihin <lolofanf1@hotmail.com>
2024-06-19 23:27:31 +0200

    display the user and registration date to the tournament in admin

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -7,7 +7,16 @@ from tournaments.models import Tournament, Competitor
 class TournamentAdmin(admin.ModelAdmin):
     list_display = ('id', 'date', 'tee_time', 'hcp_limit')
     search_fields = ('id', 'date', 'tee_time', 'course')
-    readonly_fields = ('id', 'creation_date', 'updated_date')
+    readonly_fields = ('id', 'creation_date', 'updated_date', 'display_participants_and_dates')
+
+    def display_participants_and_dates(self, obj):
+        """Create a string for the Tournament. This is required to display participant and their registration dates
+        in the list view."""
+        return '\n '.join(
+            [f'{participant.registration_date} -- {participant.competitor}' for participant
+             in obj.competitor_set.all()])
+
+    display_participants_and_dates.short_description = 'Participants (Registration Dates)'
 
 
 class CompetitorsAdmin(admin.ModelAdmin):
